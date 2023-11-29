aacc385ba7f304b8cfe4ac2bd4b0a834ab81a105
Andrew Porter <mephisto83@hotmail.com>
2023-11-28 19:07:47 -0600

    Update check.py

--- check.py
+++ check.py
@@ -1,30 +1,41 @@
 import aubio
 import numpy as np
+import os
+import json
 
-# Open the audio file
-filename = 'PRICEY_LEAD_110BPM_DMAJOR.wav'  # Replace with your audio file path
-samplerate = 0  # Use 0 to use the original samplerate of the file
-win_s = 512  # FFT size (window size)
-hop_s = win_s // 2  # Hop size
+# Set the directory containing the audio files
+directory = 'path/to/your/audio/files'  # Replace with the path to your audio files
+directory = 'C:\\Users\\bigboy\\Downloads\\Stems-20231129T005500Z-001\\Stems\\sitting_down_stems\\stems\\'  # Folder containing audio files
+output_directory = 'C:\\Users\\bigboy\\Downloads\\Stems-20231129T005500Z-001\\Stems\\sitting_down_stems\\stems\\output'  # Folder for output
 
-s = aubio.source(filename, samplerate, hop_s)
-samplerate = s.samplerate
+# Iterate through each file in the directory
+for filename in os.listdir(directory):
+    if filename.endswith(".wav"):
+        file_path = os.path.join(directory, filename)
 
-# Create an onset detection object
-onset = aubio.onset("default", win_s, hop_s, samplerate)
+        samplerate = 0  # Use original samplerate
+        win_s = 512  # FFT size
+        hop_s = win_s // 2  # Hop size
 
-# List to store onsets
-onsets = []
+        s = aubio.source(file_path, samplerate, hop_s)
+        samplerate = s.samplerate
 
-# Process the audio file
-while True:
-    samples, read = s()
-    if onset(samples):
-        print("%f" % onset.get_last_s())
-        onsets.append(onset.get_last_s())
-    if read < hop_s:
-        break
+        onset = aubio.onset("default", win_s, hop_s, samplerate)
+        onsets = []
 
-# Convert onset times from samples to seconds
-onsets_in_seconds = onsets
-print("Onsets in seconds:", onsets_in_seconds)
+        while True:
+            samples, read = s()
+            if onset(samples):
+                onsets.append(onset.get_last_s())
+            if read < hop_s:
+                break
+
+        onsets_in_seconds = onsets
+
+        # Write the onset data to a JSON file
+        json_filename = filename.replace('.wav', '.json')
+        json_path = os.path.join(directory, json_filename)
+        with open(json_path, 'w') as json_file:
+            json.dump(onsets_in_seconds, json_file)
+
+        print(f"Processed {filename}, onsets saved to {json_filename}")
